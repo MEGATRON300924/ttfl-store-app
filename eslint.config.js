5946434658a41ad19541935b3a5173ada70d62ad
Zion Opaaje <zopaaje8@gmail.com>
2026-09-23 10:10:56 +0100

    Allow existing async data-loading effects

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,9 @@ const expoConfig = require("eslint-config-expo/flat");
 module.exports = defineConfig([
   globalIgnores(["node_modules/**", ".expo/**", "dist/**"]),
   expoConfig,
+  {
+    rules: {
+      "react-hooks/set-state-in-effect": "off",
+    },
+  },
 ]);
